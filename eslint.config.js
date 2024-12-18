4375316c3d8d53b40db3a95f636043c147031540
Victor Villalobos <vpva.eien@gmail.com>
2024-12-18 12:00:59 -0300

    chore: project directory structure

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,9 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   ...mantine,
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
+  {
+    rules: {
+      'no-feature-imports': ['error', { patterns: ['@/features/*/*'] }],
+    },
+  }
 );
